48af65adda511039c39d028f371d681cf914d191
Silvio <silvio.castilo@ucr.ac.cr>
2025-11-22 23:39:06 -0600

    version serial silvio castillo

--- Cuarto proyecto/lcs.cc
+++ Cuarto proyecto/lcs.cc
@@ -11,58 +11,46 @@ LCS::~LCS() {
 
 
 string LCS::serialLCS( string& S1, string& S2 ) {
-
+    // Es el algorimto basico de LCS de programacion dinamica con matriz
+    // es una verison corregida de la puesta en el documento original 
     // inicializo la matriz de la clase como un vector 2D
     matriz = vector< vector< int >>(      
         S1.size() + 1, // cantidad de filas = largo de S1 + 1
         vector< int >( S2.size() + 1, 0 ) // cada fila es un vector de enteros del tamaño de S2 + 1, todo lleno de ceros
     );
 
-    // recorro las filas desde 1 hasta n -> tamaño de S1
-    for( int i = 1; i <= S1.size(); i++ ) {
+    int n = S1.size();
+    int m = S2.size();
 
-        // recorro las columnas desde 1 hasta m -> tamaño de S2
-        for( int j = 1; j <= S2.size(); j++ ) {
+    for (int i = 1; i<=n; i++ ){
+        for (int j = 1; i <= m; i++)
+        {
+          if(S1[i-1] == S2[j-1]){
+            matriz[i][j]=matriz[i-1][j-1] +1;
 
-            // si los caracteres en estas posiciones coinciden
-            if( S1[ i - 1 ] == S2[ j - 1 ] ) {
+          }else{
+            matriz[i][j] = max(matriz[i-1][j], matriz[i][j-1]);
 
-                // tomo la diagonal superior izquierda y le sumo 1
-                matriz[ i ][ j ] = matriz[ i - 1 ][ j - 1 ] + 1;
-            } else {
-
-                // tomo el valor maximo entre arriba y la izquierda
-                matriz[ i ][ j ] = max( matriz[ i - 1 ][ j ], matriz[ i ][ j - 1 ] );
-            }
+          }
         }
+
+
     }
+    string resultado = "";
 
-    string resultado = ""; // aqui voy armando la subsecuencia mas larga final
+    int  i= n, j=m;
 
-    int i = S1.size();
-    int j = S2.size();
-
-    while( i > 0 && j > 0 ) {
-
-         // si los caracteres actuales de S1 y S2 coinciden
-        if( S1[ i - 1 ] == S2[ j - 1 ] ) {
-
-            resultado = S1[ i - 1 ] + resultado;// agrego ese caracter al inicio de la respuesta de atras hacia adelante
-
-            // me muevo en diagonal hacia arriba a la izquierda
+    while (i > 0 && j > 0){
+        if (S1[i-1]== S2[j-1]){
+            resultado = S1[i-1] + resultado;
+            i--; j--;
+        }else if(matriz[i-1][j]> matriz[i][j-1]){
             i--;
+        }else{
             j--;
 
-        // si el valor de arriba es mayor que el de la izquierda
-        } else if( matriz[ i - 1 ][ j ] > matriz[ i ][ j - 1 ] ) {
-
-            i--; // me muevo hacia arriba
-
-        // si el valor de la izquierda es mayor o igual
-        } else {
-
-            j--; // me muevo hacia la izquierda
         }
+
     }
 
     return resultado;
